c09993d405ab57a58f8b3de588619700df5e0f37
ninja <rwasnik1112@gmail.com>
2023-12-25 20:03:57 +0530

    this is all java code of hospital

--- Drinfo.java
+++ Drinfo.java
@@ -73,3 +73,39 @@ public class DoctorInformation {
         scanner.close();
     }
 }
+
+
+import java.util.Scanner;
+
+public class PatientInformation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking input for patient's name
+        System.out.print("Enter Patient's Name: ");
+        String patientName = scanner.nextLine();
+
+        // Taking input for patient's room number
+        System.out.print("Enter Patient's Room Number: ");
+        int roomNumber = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character left in the buffer
+
+        // Taking input for type of disease
+        System.out.print("Enter Type of Disease: ");
+        String typeOfDisease = scanner.nextLine();
+
+        // Taking input for treatment period
+        System.out.print("Enter Treatment Period (e.g., 7 days): ");
+        String treatmentPeriod = scanner.nextLine();
+
+        // Displaying the entered information
+        System.out.println("\nPatient Information:");
+        System.out.println("Name: " + patientName);
+        System.out.println("Room Number: " + roomNumber);
+        System.out.println("Type of Disease: " + typeOfDisease);
+        System.out.println("Treatment Period: " + treatmentPeriod);
+
+        // Closing the scanner
+        scanner.close();
+    }
+}
